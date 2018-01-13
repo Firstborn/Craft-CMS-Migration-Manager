e9fe3a0acaf48107af9f5fd4cbc087f544dd244c
Bert Oost <bert@oostdesign.com>
2018-01-13 16:46:03 +0100

    Code style: variables

--- variables/MigrationManagerVariable.php
+++ variables/MigrationManagerVariable.php
@@ -8,35 +8,42 @@ namespace Craft;
 class MigrationManagerVariable
 {
     /**
-     * @return string
+     * @return array
      */
     public function getNewMigrations()
     {
         return craft()->migrationManager_migrations->getNewMigrations();
     }
 
+    /**
+     * @return array
+     */
     public function getAppliedMigrations()
     {
         return craft()->migrationManager_migrations->getAppliedMigrations();
     }
 
-
+    /**
+     * @return array
+     */
     public function getAssetSources()
     {
         return craft()->assetSources->getAllSources();
     }
 
+    /**
+     * @return array
+     */
     public function getAssetTransforms()
     {
         return craft()->assetTransforms->getAllTransforms();
     }
 
+    /**
+     * @return array
+     */
     public function getAllTagGroups()
     {
         return craft()->tags->getAllTagGroups();
     }
-
-
-
-
 }
